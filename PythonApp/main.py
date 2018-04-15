bebdcdb7e6e1caf53914c790ba063e69263d552f
Alejandro Vicario <a.vicarioe@gmail.com>
2018-04-15 18:06:47 +0200

    Python first commit

---
+++ PythonApp/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import tkinter as Tk
+
+class App(Tk.Frame):
+
+	def __init__(self, master=None):
+		super(App, self).__init__(master)
+		self.bind("<Configure>", self.on_resize)
+		self.createWidgets()
+
+	def createWidgets(self):
+		self.rowconfigure(0, weight=1)
+
+		self.button = Tk.Button(self)
+		self.button["text"] = "QUIT"
+		self.button["fg"] = "red"
+		self.button["command"] = self.quit
+
+		self.button.grid(row=0, column=1)
+		self.columnconfigure(0, weight=1)
+
+		self.hi_there = Tk.Button(self, text="Hello", command=self.say_hi)
+		self.hi_there.grid(row=0, column=0)
+		self.columnconfigure(1, weight=1)
+
+	def say_hi(self):
+		print("Hello, World!")
+
+	def on_resize(self,event):
+		print(self.grid_bbox())
+
+
+def main():
+	root = Tk.Tk()
+	root.rowconfigure(0, weight=1)
+	root.columnconfigure(0, weight=1)
+	app = App(root)
+	app.grid(sticky=Tk.N+Tk.S+Tk.E+Tk.W)
+	root.mainloop()
+	root.destroy()
+
+if __name__ == "__main__":
+	# execute only if run as a script
+	main()
